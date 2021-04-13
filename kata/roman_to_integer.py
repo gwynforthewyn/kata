f20e15b62cc791454d3ad9ee837e2908fddb566f
Gwyn <gwyn@playtechnique.io>
2021-04-12 20:39:40 -0600

    Roman to integer works!

--- kata/roman_to_integer.py
+++ kata/roman_to_integer.py
@@ -1,22 +1,28 @@
+#https://leetcode.com/problems/roman-to-integer/
+
 class Solution:
     def romanToInt(self, s: str) -> int:
-        result = 0
-        
-        for char in s:
-          result += self.lookup(char)
-        
-        print(result)
-        return result
+
+      result = 0
+
+      for i in range(len(s)):
+
+        if (i != len(s)-1 and self.lookup(s[i]) < self.lookup(s[i + 1])):
+          result -= self.lookup(s[i])
+        else:
+          result += self.lookup(s[i])
+      
+      return result
         
     def lookup(self, candidate):
         romans = {
-                  "I": 1,
-                  "V": 5,
-                  "X": 10,
-                  "L": 50,
-                  "C": 100,
-                  "D": 500,
-                  "M": 1000
+                  "I" : 1,
+                  "V" : 5,
+                  "X" : 10,
+                  "L" : 50,
+                  "C" : 100,
+                  "D" : 500,
+                  "M" : 1000
                  }
         
         return romans[candidate]
--- tests/test_roman_to_integer.py
+++ tests/test_roman_to_integer.py
@@ -1,5 +1,17 @@
 from kata.roman_to_integer import Solution
 
-def test_number_three():
+def test_three():
     foo = Solution()
     assert foo.romanToInt("III") == 3
+
+def test_four():
+    foo = Solution()
+    assert foo.romanToInt("IV") == 4
+
+def test_fifty_eight():
+    foo = Solution()
+    assert foo.romanToInt("LVIII") == 58
+
+def test_nineteen_ninety_four():
+    foo = Solution()
+    assert foo.romanToInt("MCMXCIV") == 1994
