4ccb7fd5b0042654af6ba64cb6ee273fa4d59560
Gwyn <gwyn@playtechnique.io>
2021-04-12 19:03:44 -0600

    Adds scaffolding for katas to run with pytest.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='kata',
+      version='0.0',
+      description='practice problems',
+      author='gwyn',
+      author_email='gwyn@playtechnique.io',
+      license='MIT',
+      packages=['kata'],
+      zip_safe=False)
